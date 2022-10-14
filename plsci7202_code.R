c63deb62aadb2e0ad9dfaafab7f29e7189fe4281
kb594 <114926970+kb594@users.noreply.github.com>
2022-10-14 08:03:15 -0500

    Add files via upload

--- plsci7202_code.R
+++ plsci7202_code.R
@@ -7,3 +7,15 @@ missingdata_function <- function(x) {
 sample <- c(5,5,5,5,NA)
 
 missingdata_function(sample)
+
+#creating a new function to apply the above function to an entire dataframe
+
+#changing the previously used sample to have two columns
+sample2 <- cbind(sample,sample)
+
+missing_dataframe <- function(x) {
+  sapply(as.data.frame(x),missingdata_function)
+}
+
+#testing the new function
+missing_dataframe(sample2)
